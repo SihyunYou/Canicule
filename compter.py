8bdca6fdd6297249a188346a27b72cdcba856fc9
Sihyun, You <Jynis2937@gmail.com>
2022-05-24 01:51:59 +0900

    df tabulate

--- compter.py
+++ compter.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import pandas as pd
+import locale
+locale.setlocale(locale.LC_ALL, 'en_US')
 
 somme = {}
 somme_profit = {}
@@ -25,37 +28,45 @@ for p in range(len(lines)):
             somme[qui] = montant_recu
 
         if p + 1 < len(lines) and lines[p + 1][0] != '\t' or p + 1 == len(lines):
-            print("------------")
+            list_rangee = []
+
             for key in somme:
                 t = int(somme[key] * profitabilite / 100)
-                print(key + ' : '+ str(t))
                 if key in somme_profit.keys():
                     somme_profit[key] += t
                 else:
                     somme_profit[key] = t
-            print("------------")
-            print(somme)
-            print(somme_profit)
-            print("총원금 : " + str(sum(somme.values())))
-            print("총 평가손익 : " + str(sum(somme_profit.values())))
+
+            for key in somme:
+                rangee = []
+                rangee.append(key)
+                rangee.append(locale.format_string("%d", int(somme[key] * profitabilite / 100), grouping=True))
+                rangee.append(locale.format_string("%d", somme[key], grouping=True))
+                rangee.append(locale.format_string("%d", somme_profit[key], grouping=True))
+                list_rangee.append(rangee)
+            
+            print("누적 원금 : " + locale.format_string("%d", sum(somme.values()), grouping=True) + "원")
+            print("누적 평가손익 : " + locale.format_string("%d", sum(somme_profit.values()), grouping=True) + "원")
+
+            df = pd.DataFrame(list_rangee, columns = ['채권자', '당해 평가손익', '원금 합계', '평가손익 합계'])
+            print(df.to_markdown()) 
             print('\n')
     else:
         list_donnee = lines[p].split(' ')
         date_initial = list_donnee[0]
-        event = int(list_donnee[1])
-        montant_initial = int(list_donnee[2])
-        montant_final = int(list_donnee[3])
+        date_final = list_donnee[1]
+        event = int(list_donnee[2])
+        montant_initial = int(list_donnee[3])
+        montant_final = int(list_donnee[4])
 
-        print(date_initial)
+        print(date_initial + ' ~ ' + date_final)
         if event == 1:
-            print("입금")
+            print("이벤트 : 입금")
         elif event == 2:
-            print("운용자 임의")
+            print("이벤트 : 운용자 임의")
         elif event == 3:
-            print("만기 도래")
-        print("초기운용금액 : " + str(montant_initial))
-        print("말기운용금액 : " + str(montant_final))
-
+            print("이벤트 : 만기 도래")
+        print("운용금액(초기/말기) : " + locale.format_string("%d", montant_initial, grouping=True) + '원 ~ ' + locale.format_string("%d", montant_final, grouping=True) + '원')
 
         evaluation = montant_final - montant_initial
         profitabilite = evaluation / montant_initial * 100
@@ -69,7 +80,7 @@ for p in range(len(lines)):
                 else:
                     somme_profit[key] = t
 
-        print("전이벤트대비 평가손익 : " + str(evaluation))
+        print("전이벤트대비 평가손익 : " + locale.format_string("%d", evaluation, grouping=True) + '원')
         print("수익률 : " + str(round(profitabilite, 6)) + "%")
 
         if event >= 2:
