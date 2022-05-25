29fb85f309135fdf2e19fc5bf783910c69a4987b
Ilya <Caesar2937@gmail.com>
2022-05-25 21:29:02 +0900

    datetime & timestamp

--- compter.py
+++ compter.py
@@ -2,13 +2,21 @@
 import pandas as pd
 import locale
 locale.setlocale(locale.LC_ALL, 'en_US')
+import datetime
+from datetime import datetime
+from datetime import timedelta
+import time
 
 somme = {}
 somme_profit = {}
+date_initial = 0
+date_final = 0 
 
 f = open('profit_2022_1.txt', 'r', encoding='UTF-8')
 _lines = f.readlines()
 lines = []
+i = 0
+
 for p in range(len(_lines)):
     if _lines[p][0] != '#':
         if _lines[p][-1] == '\n':
@@ -54,18 +62,23 @@ for p in range(len(lines)):
     else:
         list_donnee = lines[p].split(' ')
         date_initial = list_donnee[0]
+        temp_initial = datetime.strptime(date_initial, '%Y.%m.%d.%H:%M')
         date_final = list_donnee[1]
+        temp_final = datetime.strptime(date_final, '%Y.%m.%d.%H:%M')
+        diff_timestamp = int(time.mktime(temp_final.timetuple()) - time.mktime(temp_initial.timetuple()))
+
         event = int(list_donnee[2])
         montant_initial = int(list_donnee[3])
         montant_final = int(list_donnee[4])
 
-        print(date_initial + ' ~ ' + date_final)
+        print(date_initial + ' ~ ' + date_final + ' (' + str(temp_final - temp_initial) + ', time_diff=' + str(diff_timestamp) + ')' )
+        i += 1
         if event == 1:
-            print("이벤트 : 입금")
+            print("이벤트 " + str(i) + " : 입출금")
         elif event == 2:
-            print("이벤트 : 운용자 임의")
+            print("이벤트 " + str(i) + " : 운용자 임의")
         elif event == 3:
-            print("이벤트 : 만기 도래")
+            print("이벤트 " + str(i) + " : 만기 도래")
         print("운용금액(초기/말기) : " + locale.format_string("%d", montant_initial, grouping=True) + '원 ~ ' + locale.format_string("%d", montant_final, grouping=True) + '원')
 
         evaluation = montant_final - montant_initial
@@ -81,8 +94,9 @@ for p in range(len(lines)):
                     somme_profit[key] = t
 
         print("전이벤트대비 평가손익 : " + locale.format_string("%d", evaluation, grouping=True) + '원')
+        print("시간당 평균 평가손익 : " + locale.format_string("%d", evaluation / diff_timestamp * 3600, grouping=True) + '원')
         print("수익률 : " + str(round(profitabilite, 6)) + "%")
-
+        #print("시간당 평균 수익률 : " + locale.format_string("%f", profitabilite / diff_timestamp * 3600, grouping=True) + '%')
         if event >= 2:
             print('\n')
 
