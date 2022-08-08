128e9f7445b778d3ca6a5a8e1c6ab875b0d9fbe4
Ilya <Caesar2937@gmail.com>
2022-08-08 21:00:16 +0900

    Classifier ExaminerCompte

--- trader.py
+++ trader.py
@@ -199,6 +199,7 @@ class Acheter:
 		time.sleep(TEMPS_DORMIR)
 
 
+COMTE = 200
 class RecupererInfoCandle:
 	def __recuperer_array(self, _s, _n):
 		arr = np.zeros(_n)
@@ -209,7 +210,7 @@ class RecupererInfoCandle:
 	def __init__(self, _symbol):
 		querystring = {
 			"market" : "KRW-" + _symbol,
-			"count" : '200'
+			"count" : str(COMTE)
 		}
 
 		try:
@@ -220,12 +221,13 @@ class RecupererInfoCandle:
 			imprimer(Niveau.EXCEPTION, "Rate de recuperer les donnes de prix.")
 			raise Exception("RecupererInfoCandle")
 
-		self.array_opening_price = self.__recuperer_array('opening_price', 200)
-		self.array_trade_price = self.__recuperer_array('trade_price', 200)
+		self.array_opening_price = self.__recuperer_array('opening_price', COMTE)
+		self.array_trade_price = self.__recuperer_array('trade_price', COMTE)
 		self.prix_courant = self.array_trade_price[-1]
-		self.array_high_price = self.__recuperer_array('high_price', 200)
-		self.array_low_price = self.__recuperer_array('low_price', 200)
-		self.array_acc_trade_price = self.__recuperer_array('candle_acc_trade_price', 200)
+		self.array_high_price = self.__recuperer_array('high_price', COMTE)
+		self.array_low_price = self.__recuperer_array('low_price', COMTE)
+		self.array_acc_trade_price = self.__recuperer_array('candle_acc_trade_price', COMTE)
+
 
 class Verifier:
 	def __init__(self, _symbol):
@@ -239,14 +241,14 @@ class Verifier:
 		p = self.candle.array_trade_price[-20]
 		q = self.candle.prix_courant
 		if - 0.1 < (q - p) / self.candle.prix_courant < 0.25 and \
-			self.candle.prix_courant < self.mm20 - self.std20 * 0.5243:
+			self.candle.prix_courant < self.mm20 - self.std20 * 0.2533: # 0.2533(10%), 0.5243(20%)
 			return True
 		return False
 
 	def verifier_prix(self):
-		if 0.050 < self.candle.prix_courant < 0.0995 or 0.5 < self.candle.prix_courant < 0.995 or \
-			5 < self.candle.prix_courant < 9.95 or 50 < self.candle.prix_courant < 99.5 or \
-			500 < self.candle.prix_courant < 995 or 4000 < self.candle.prix_courant:
+		if 0.048 < self.candle.prix_courant < 0.0995 or 0.48 < self.candle.prix_courant < 0.995 or \
+			4.8 < self.candle.prix_courant < 9.95 or 48 < self.candle.prix_courant < 99.5 or \
+			480 < self.candle.prix_courant < 995 or 3600 < self.candle.prix_courant:
 			return True
 		return False
 
@@ -296,29 +298,6 @@ class Verifier:
 						"Hors d'envelope ! decalage : " + str(round(decalage, 3)))
 			return True
 		return False
-
-
-DERNIER_SYMBOL = ''
-def controler_achats(_symbol, _somme_totale): # 전부매집
-	try:
-		v = Verifier(_symbol)
-		if v.verfier_surete() and v.verifier_prix():
-			if v.verifier_bb_variable(20) or v.verifier_vr(20) or v.verifier_decalage_mm(20, 2):
-				a = Acheter(_symbol, v.candle.prix_courant, _somme_totale)
-				#a.diviser_lineaire(0.3333, 36, 10000000) # 선형 매집
-				#a.diviser_exposant(0.38, 29, 1.2) # 지수 매집
-				#a.diviser_parabolique(0.3333, 25) # 제1형 포물선 매집
-				a.diviser_parabolique2(0.3333, 27) # 제2형 포물선 매집
-				#a.diviser_lapin(0.34, 16) # 토끼 매집
-			
-				imprimer(Niveau.INFORMATION, "Acheve de demander l'achat \'" + _symbol + '\'')
-				t = threading.Thread(target = winsound.Beep, args=(440, 500))
-				t.start()
-
-				return True
-	except Exception as e:
-		traceback.print_exc()
-	return False
 
 
 class Annuler:
@@ -376,36 +355,47 @@ class Annuler:
 		time.sleep(TEMPS_DORMIR)
 		
 
-def examiner_compte():
-	payload = {
-		'access_key': CLE_ACCES,
-		'nonce': str(uuid.uuid4()),
-	}
+class ExaminerCompte:
+	def __init__(self):
+		payload = {
+			'access_key': CLE_ACCES,
+			'nonce': str(uuid.uuid4()),
+		}
 
-	jwt_token = jwt.encode(payload, CLE_SECRET)
-	authorize_token = 'Bearer {}'.format(jwt_token)
-	headers = {"Authorization": authorize_token}
+		jwt_token = jwt.encode(payload, CLE_SECRET)
+		authorize_token = 'Bearer {}'.format(jwt_token)
+		headers = {"Authorization": authorize_token}
 
-	while(True):
-		try:
-			response = requests.get(URL_SERVEUR + "/v1/accounts", headers=headers)
-			dict_response = json.loads(response.text)
-			time.sleep(TEMPS_DORMIR)
+		while(True):
+			try:
+				response = requests.get(URL_SERVEUR + "/v1/accounts", headers=headers)
+				self.dict_response = json.loads(response.text)
+				time.sleep(TEMPS_DORMIR)
+			except:
+				time.sleep(TEMPS_DORMIR)
 
-			#print(dict_response)
-			return dict_response
-		except:
-			time.sleep(TEMPS_DORMIR)
+	def recuperer_solde_krw(self):
+		for mon_dict in self.dict_response:
+			if mon_dict.get('currency') == "KRW":
+				return float(mon_dict.get('balance'))
+		return 0
 
-def examiner_symbol_compte(_symbol):
-	for mon_dict in examiner_compte():
-		if(mon_dict.get('currency') == _symbol):
-			balance = float(mon_dict.get('balance'))
-			locked = float(mon_dict.get('locked'))
-			avg_buy_price = float(mon_dict.get('avg_buy_price'))
+	def recuperer_symbols(self):
+		symbols = []
+		for mon_dict in self.dict_response:
+			symbols.append(mon_dict.get('currency'))
+		return symbols
 
-			return balance, locked, avg_buy_price
-	return -1, -1, -1
+	def recuperer_symbol_info(self, _symbol):
+		for mon_dict in self.dict_response:
+			if mon_dict.get('currency') == _symbol:
+				balance = float(mon_dict.get('balance'))
+				locked = float(mon_dict.get('locked'))
+				avg_buy_price = float(mon_dict.get('avg_buy_price'))
+
+				return balance, locked, avg_buy_price
+		return -1, -1, -1
+
 
 # flag_commande_vendre는 매도주문이 걸려 있는지 여부에 대한 플래그
 PRIX_MINIMUM_VENDU = 5000
@@ -415,7 +405,8 @@ count_montant_insuffissant = 0
 def est_commande_vente_complete(_symbol):
 	global flag_commande_vendre
 	global count_montant_insuffissant
-	dict_response = examiner_compte()
+	ec = ExaminerCompte()
+	symbols = ec.recuperer_symbols()
 
 	if(count_montant_insuffissant > 300):
 		imprimer(Niveau.AVERTISSEMENT, "Annuler le vente car le reste de demande de vente n'est pas conclu.")
@@ -423,12 +414,9 @@ def est_commande_vente_complete(_symbol):
 		flag_commande_vendre = False
 		return True
 
-	for mon_dict in dict_response:
+	for symbol in symbols:
 		try:
-			currency = mon_dict.get('currency')
-			balance = float(mon_dict.get('balance'))
-			locked = float(mon_dict.get('locked'))
-			avg_buy_price = float(mon_dict.get('avg_buy_price'))
+			balance, locked, avg_buy_price = ec.recuperer_symbol_info(symbol)
 			montant = (balance + locked) * avg_buy_price
 		except:
 			time.sleep(TEMPS_DORMIR)
@@ -486,12 +474,33 @@ def vendre_biens(_symbol, _volume, _prix):
 	#print(dict_response)
 	return dict_response
 
+def controler_achats(_symbol, _somme_totale): # 전부매집
+	try:
+		v = Verifier(_symbol)
+		if v.verfier_surete() and v.verifier_prix():
+			if v.verifier_bb_variable(20) or v.verifier_vr(20) or v.verifier_decalage_mm(20, 2):
+				a = Acheter(_symbol, v.candle.prix_courant, _somme_totale)
+				#a.diviser_lineaire(0.3333, 36, 10000000) # 선형 매집
+				#a.diviser_exposant(0.38, 29, 1.2) # 지수 매집
+				#a.diviser_parabolique(0.3333, 25) # 제1형 포물선 매집
+				a.diviser_parabolique2(0.3333, 27) # 제2형 포물선 매집
+				#a.diviser_lapin(0.34, 16) # 토끼 매집
+			
+				imprimer(Niveau.INFORMATION, "Acheve de demander l'achat \'" + _symbol + '\'')
+				t = threading.Thread(target = winsound.Beep, args=(440, 500))
+				t.start()
+
+				return True
+	except Exception as e:
+		traceback.print_exc()
+	return False
+
 def controler_vente(_symbol, _somme_totale, _proportion_profit):
 	global flag_commande_vendre
 	global count_montant_insuffissant
 
 	try:
-		balance, locked, avg_buy_price = examiner_symbol_compte(_symbol)
+		balance, locked, avg_buy_price = ExaminerCompte().recuperer_symbol_info(_symbol)
 		if(balance < 0):
 			return False
 		
@@ -504,10 +513,8 @@ def controler_vente(_symbol, _somme_totale, _proportion_profit):
 				time.sleep(TEMPS_DORMIR)
 			time.sleep(TEMPS_DORMIR)
 
-			balance, locked, avg_buy_price = examiner_symbol_compte(_symbol)
-			proportion_supplement = 0
-			proportion_vente = _proportion_profit + proportion_supplement
-			imprimer(Niveau.INFORMATION, "prix de moyenne d'acaht : " + str(avg_buy_price) + ", position de vente : " + str(tailler(avg_buy_price, -1 * proportion_vente)))
+			balance, locked, avg_buy_price = ExaminerCompte().recuperer_symbol_info(_symbol)
+			imprimer(Niveau.INFORMATION, "prix de moyenne d'acaht : " + str(avg_buy_price) + ", position de vente : " + str(tailler(avg_buy_price, -1 * _proportion_profit)))
 			vendre_biens(_symbol, balance + locked, tailler(avg_buy_price, -1 * proportion_vente))
 
 			flag_commande_vendre = True
@@ -543,8 +550,8 @@ def obtenir_list_symbol():
 				imprimer(Niveau.ERREUR, "Rate de recuperer la liste de symbols. (2)")
 				raise Exception('RecupererInfoCandle')
 
-			if 0.05 < r.prix_courant < 0.096 or 0.5 < r.prix_courant < 0.96 or 5 < r.prix_courant < 9.6 or \
-				50 < r.prix_courant < 96 or 500 < r.prix_courant < 960 or 4000 < r.prix_courant:
+			if 0.04 < r.prix_courant < 0.102 or 0.4 < r.prix_courant < 1.02 or 4 < r.prix_courant < 10.2 or \
+				40 < r.prix_courant < 102 or 400 < r.prix_courant < 1020 or 3200 < r.prix_courant:
 				acc_trade_price = 0
 				for i in range(80):
 					acc_trade_price += r.array_acc_trade_price[i]
@@ -552,19 +559,11 @@ def obtenir_list_symbol():
 				if acc_trade_price > 300000000: #300백만
 					list_symbol.append(market[4:])
 
-	global DERNIER_SYMBOL
-	DERNIER_SYMBOL = list_symbol[-1]
 	imprimer(Niveau.INFORMATION, 
 				"Monitorer la liste suivie de crypto monnaies qui suffit a la critere d'achat.\n" + \
 				'[' + ', '.join(list_symbol) + ']')	
-
 	return list_symbol
 
-def obtenir_solde_KRW():
-	for mon_dict in examiner_compte():
-		if mon_dict.get('currency') == "KRW":
-			return float(mon_dict.get('balance'))
-	return 0
 
 idx = 0
 def animater(_s):
@@ -587,7 +586,7 @@ if __name__=="__main__":
 	parser.add_argument('-s', type=int, required=False, help='-s : La somme mise.')
 	args = parser.parse_args()
 	
-	Sp = S = obtenir_solde_KRW()
+	Sp = S = ExaminerCompte().recuperer_solde_krw()
 	imprimer(Niveau.INFORMATION, "KRW disponible : " + format(int(S), ','))
 	list_symbol = obtenir_list_symbol()
 
@@ -610,7 +609,6 @@ if __name__=="__main__":
 			if nom_symbol != '' and nom_symbol in list_symbol:
 				list_symbol.remove(nom_symbol)
 				list_symbol.insert(0, nom_symbol)
-				DERNIER_SYMBOL = list_symbol[-1]
 		else:
 			breakable, flag_commande_vendre = False, False
 			fault = 0
@@ -632,7 +630,6 @@ if __name__=="__main__":
 
 			list_symbol.remove(nom_symbol)
 			list_symbol.insert(0, nom_symbol)
-			DERNIER_SYMBOL = list_symbol[-1]
 
 			while True:
 				if est_commande_vente_complete(nom_symbol):
@@ -649,7 +646,7 @@ if __name__=="__main__":
 
 			Annuler().annuler_achats()
 
-			S = int(obtenir_solde_KRW())
+			S = int(ExaminerCompte().recuperer_solde_krw())
 			interet = "Interet : " + '{0:+,}'.format(int(S - Sp)) + ' (' + str(datetime.now() - TEMPS_INITIAL) + ')'
 			imprimer(Niveau.INFORMATION, interet)
 			S = int(S * Commission)
